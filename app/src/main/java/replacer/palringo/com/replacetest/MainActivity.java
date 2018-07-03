68eccbe3eb936acf3cf666fedd99ffa2fcb52230
bradley fail <bradley.fail@palringo.com>
2018-07-03 13:50:46 +0100

    added log

--- app/src/main/java/replacer/palringo/com/replacetest/MainActivity.java
+++ app/src/main/java/replacer/palringo/com/replacetest/MainActivity.java
@@ -2,6 +2,7 @@ package replacer.palringo.com.replacetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this,"bye",Toast.LENGTH_LONG).show();
+
+
+        Log.d("MainActivity", "Hello");
     }
 }
