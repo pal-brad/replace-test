01a4750a574db3845b02b2b931ed52fe2e342ea9
bradley fail <bradley.fail@palringo.com>
2018-07-03 13:49:43 +0100

    created android project

---
+++ app/src/main/java/replacer/palringo/com/replacetest/MainActivity.java
@@ -0,0 +1,13 @@
+package replacer.palringo.com.replacetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
